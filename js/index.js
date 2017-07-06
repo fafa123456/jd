eca7c6e79c73414f1e5b4fa5b79d5a32b3216ed6
皮皮虾 <you@example.com>
2017-07-06 18:59:36 +0800

    给div注册了事件

---
+++ js/index.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    document.querySelector(".div1").onclick = function () {
+        console.log('123');
+
+        document.querySelector(".div1").style.backgroundColor = "pink";
+    }
+}
